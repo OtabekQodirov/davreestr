3acdb9c17cd7382a078af0d54280def8ad0647fc
MOHIR <mohirkarimov4181@gmail.com>
2022-07-05 12:30:39 +0500

    add docs fragment

--- app/src/main/java/com/example/myapplication/app/activity/MainActivity.kt
+++ app/src/main/java/com/example/myapplication/app/activity/MainActivity.kt
@@ -2,11 +2,19 @@ package com.example.myapplication.app.activity
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
 import com.example.myapplication.R
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+
+        /* val view: View = LayoutInflater.from(this).inflate(R.layout.item_document, null)
+
+         findViewById<ViewGroup>(R.id.frameLayout).addView(view)*/
     }
 }
