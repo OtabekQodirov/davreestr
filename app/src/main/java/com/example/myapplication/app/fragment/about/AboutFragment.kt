67381751ffb3e123ad59776c1934624419cd7be1
Muxsinbek <75078604+Muxsinbek@users.noreply.github.com>
2022-07-05 01:07:46 -0700

    add map implementation

--- app/src/main/java/com/example/myapplication/app/fragment/about/AboutFragment.kt
+++ app/src/main/java/com/example/myapplication/app/fragment/about/AboutFragment.kt
@@ -8,8 +8,21 @@ import androidx.fragment.app.Fragment
 import com.example.myapplication.R
 import com.example.myapplication.databinding.FragmentAboutBinding
 import com.example.myapplication.databinding.FragmentMainBinding
+import com.google.android.gms.maps.CameraUpdateFactory
+import com.google.android.gms.maps.OnMapReadyCallback
+import com.google.android.gms.maps.SupportMapFragment
+import com.google.android.gms.maps.model.LatLng
+import com.google.android.gms.maps.model.MarkerOptions
 
 class AboutFragment : Fragment() {
+    private val callback = OnMapReadyCallback { googleMap ->
+
+        val uzb = LatLng(41.0, 64.0)
+        googleMap.addMarker(MarkerOptions().position(uzb).title("Marker in Uzb"))
+        //
+        googleMap.moveCamera(CameraUpdateFactory.newLatLng(uzb))
+    }
+
     override fun onCreateView(
         inflater: LayoutInflater,
         container: ViewGroup?,
@@ -18,6 +31,8 @@ class AboutFragment : Fragment() {
         val binding = FragmentAboutBinding.inflate(inflater, container, false)
 
         //NOTE: WRITE HERE
+        val mapFragment = childFragmentManager.findFragmentById(R.id.google_map) as SupportMapFragment?
+        mapFragment?.getMapAsync(callback)
 
         return binding.root
     }
