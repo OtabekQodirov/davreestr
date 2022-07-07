6ed9605d43ca78f16cf7fd372fbc724b9b56efa8
OtabekQodirov <oquz2311@gmail.com>
2022-07-07 12:31:11 +0500

    main page tugadi

--- app/src/main/java/com/example/myapplication/app/adapter/ViewPagerAdapter.kt
+++ app/src/main/java/com/example/myapplication/app/adapter/ViewPagerAdapter.kt
@@ -11,19 +11,38 @@ class ViewPagerAdapter(fragment: Fragment) : FragmentStateAdapter(fragment) {
         R.string.vp_1_title,
         R.string.vp_2_title,
         R.string.vp_3_title,
-        R.string.vp_4_title
+        R.string.vp_4_title,
+        R.string.vp_5_title,
+        R.string.vp_6_title,
+        R.string.vp_7_title,
+        R.string.vp_8_title,
+        R.string.vp_9_title,
+        R.string.vp_10_title
     )
     private val desc = arrayListOf(
         R.string.vp_1_detail,
         R.string.vp_2_detail,
         R.string.vp_3_detail,
-        R.string.vp_4_detail
+        R.string.vp_4_detail,
+        R.string.vp_5_detail,
+        R.string.vp_6_detail,
+        R.string.vp_7_detail,
+        R.string.vp_8_detail,
+        R.string.vp_9_detail,
+        R.string.vp_10_detail
+
     )
     private val icon = arrayListOf(
-        R.drawable.add_ic,
-        R.drawable.cad_search,
-        R.drawable.ic_clock,
-        R.drawable.ic_references
+        R.drawable.home_ic,
+        R.drawable.prohibition_ic,
+        R.drawable.value_ic,
+        R.drawable.shaping_ic,
+        R.drawable.ic_duplicate,
+        R.drawable.ic_duplicate,
+        R.drawable.ic_doc,
+        R.drawable.ic_registration,
+        R.drawable.ic_area,
+        R.drawable.ic_registration,
     )
 
     override fun getItemCount() = title.size
