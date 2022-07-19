40a9ea3e29afdc576797bb799f46946f7d30d01a
Muxsinbek <75078604+Muxsinbek@users.noreply.github.com>
2022-07-19 03:08:34 -0700

    some changes

--- app/src/main/java/com/example/myapplication/app/fragment/main/MainFragment.kt
+++ app/src/main/java/com/example/myapplication/app/fragment/main/MainFragment.kt
@@ -4,24 +4,45 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Toast
 import androidx.fragment.app.Fragment
+import androidx.lifecycle.ViewModelProvider
+import androidx.lifecycle.get
 import com.example.myapplication.R
 import com.example.myapplication.app.adapter.ViewPagerAdapter
+import com.example.myapplication.data.remote.ResponseBean
 import com.example.myapplication.databinding.FragmentEcommerceBinding
 import com.example.myapplication.databinding.FragmentMainBinding
+import com.example.myapplication.domain.ApiCallback
 
-class MainFragment : Fragment() {
+class MainFragment : Fragment(), ApiCallback {
     override fun onCreateView(
         inflater: LayoutInflater,
         container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View {
         val binding = FragmentMainBinding.inflate(inflater, container, false)
+        val viewModel = ViewModelProvider(this)[MainViewModel::class.java]
 
         //NOTE: WRITE HERE
         val adapter = ViewPagerAdapter(this)
         binding.viewPager2.adapter = adapter
 
+
+        binding.cadSearch.setOnClickListener {
+            viewModel.getCadInfo("31231", this)
+        }
+
         return binding.root
+    }
+
+    override fun onSuccess(response: ResponseBean) {
+        // TODO:
+        Toast.makeText(context, response.address, Toast.LENGTH_SHORT).show()
+    }
+
+    override fun onFailure(message: String) {
+        // TODO:
+        Toast.makeText(context, message, Toast.LENGTH_SHORT).show()
     }
 }
--- app/src/main/java/com/example/myapplication/domain/Repository.kt
+++ app/src/main/java/com/example/myapplication/domain/Repository.kt
@@ -1,10 +1,14 @@
 package com.example.myapplication.domain
 
 import android.content.Context
+import android.os.Handler
+import android.os.Looper
 import com.example.myapplication.data.local.AppDatabase
 import com.example.myapplication.data.remote.RequestBean
 import com.example.myapplication.data.remote.ResponseBean
 import com.example.myapplication.data.remote.RetrofitClient
+import com.google.gson.Gson
+import com.google.gson.JsonObject
 import retrofit2.Call
 import retrofit2.Callback
 import retrofit2.Response
@@ -15,6 +19,35 @@ class Repository(context: Context) {
     val database: AppDatabase = AppDatabase.init(context)
 
     fun searchInfo(cadNumber: String, result: ApiCallback) {
+
+        val str1 = """
+            {
+                "address": "г. Ташкент, Яккасарайский, Дамарик МФЙ, Кичик Бешегоч кучаси, 3а, 3б-уй",
+                "area": 7000,
+                "area_bino": 803,
+                "area_jilaya": 22.5,
+                "ban_info": "105-000625 - 2018-11-23",
+                "cad_number": "10:05:02:02:04:0037",
+                "code": 99,
+                "cost": "0",
+                "count_subjects": 1,
+                "date": "",
+                "district_id": 1726287,
+                "has_ban": "1",
+                "number": "",
+                "object_type": "3",
+                "region_id": 1726,
+                "subjects": ["4239565cb4b4935be0b944ec1d1639d4", "4239565cb4b4935be0b944ec1"]
+            }
+        """
+
+        val responseV1 = Gson().fromJson(str1, ResponseBean::class.java)
+
+
+        Handler(Looper.myLooper()!!).postDelayed({ result.onSuccess(responseV1) }, 2_000)
+
+
+        /*
         val request = RequestBean(cad_num = cadNumber)
         val call = retrofit.api.getCadInfo(request)
 
@@ -37,7 +70,7 @@ class Repository(context: Context) {
 
         })
 
-
+        */
     }
 
     fun getAllDocuments() = database.documentDao().getAll()
